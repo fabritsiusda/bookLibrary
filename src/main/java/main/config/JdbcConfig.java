a22b306b31f756433c7a15844ea9b25f8a5872c8
fabritsiusda <fabritsiusda@gmail.com>
2020-11-16 11:31:35 +0500

    fix add book, now returned book

--- src/main/java/main/config/JdbcConfig.java
+++ src/main/java/main/config/JdbcConfig.java
@@ -2,16 +2,12 @@ package main.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan("main")
-@EnableWebMvc
 public class JdbcConfig {
 
     @Value("${db.username}")
--- src/main/java/main/dao/BookDAO.java
+++ src/main/java/main/dao/BookDAO.java
@@ -3,8 +3,11 @@ package main.dao;
 import main.models.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Service
@@ -34,8 +37,17 @@ public class BookDAO implements BookRepository {
 
     @Override
     public Book addBook(Book book) {
-        int id = jdbcTemplate.update("insert into book (title, author, description) values(?, ?, ?)",
-                book.getTitle(), book.getAuthor(), book.getDescription());
-        return null;
+        GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            String sql = "insert into book (title, author, description) values(?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, book.getTitle());
+            st.setString(2, book.getAuthor());
+            st.setString(3, book.getDescription());
+            return st;
+        }, holder);
+        long id = holder.getKey().longValue();
+        book.setId(id);
+        return book;
     }
 }
